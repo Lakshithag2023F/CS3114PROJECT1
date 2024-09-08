b23b38f1c66b13cb41a234822f74a95a824c8a08
Lakshithag2023F <148505659+Lakshithag2023F@users.noreply.github.com>
2024-09-07 22:57:03 -0400

    Update DoubleLL.java

--- src/DoubleLL.java
+++ src/DoubleLL.java
@@ -1,8 +1,16 @@
 public class DoubleLL {
     //~ Fields ................................................................
+    private int size;
 
     //~ Constructors ..........................................................
-
+    public DoubleLL()
+    {
+        init();
+    }
     //~Public  Methods ........................................................
-
+    private void init()
+    {
+        
+        size = 0;
+    }
 }
