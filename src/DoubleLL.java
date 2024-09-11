8e64d636a44d5708ec96a06cda74b9b2dbf60162
Lakshithag2023F <148505659+Lakshithag2023F@users.noreply.github.com>
2024-09-10 22:03:20 -0400

    stuff

--- src/DoubleLL.java
+++ src/DoubleLL.java
@@ -13,4 +13,17 @@ public class DoubleLL {
         
         size = 0;
     }
+    
+    
+    /**
+     * Sets the node before this one
+     *
+     * @param n
+     *            the node before this one
+     */
+    public void setPrevious(Node<E> n)
+    {
+        previous = n;
+    }
+
 }
--- src/Node.java
+++ src/Node.java
@@ -1,88 +1,26 @@
-public class Node<E> {
+public class Node {
     
     // ----------------------------------------------------------
     
     //~ Fields ................................................................
-
+    private int index;
     //~ Constructors ..........................................................
 
+    public Node(int indexNUM)
+    {
+        index = indexNUM;
+    }
+    
     //~Public  Methods ........................................................
 
-
-    private Node<E> next;
-    private Node<E> previous;
-    private E data;
-
-    /**
-     * Creates a new node with the given data
-     *
-     * @param d
-     *            the data to put inside the node
-     */
-    public Node(E d)
+    public int setIndex()
     {
-        data = d;
+        
+        return index;
+        
     }
 
-
-    /**
-     * Sets the node after this node
-     *
-     * @param n
-     *            the node after this one
-     */
-    public void setNext(Node<E> n)
-    {
-        next = n;
-    }
-
-
-    /**
-     * Sets the node before this one
-     *
-     * @param n
-     *            the node before this one
-     */
-    public void setPrevious(Node<E> n)
-    {
-        previous = n;
-    }
-
-
-    /**
-     * Gets the next node
-     *
-     * @return the next node
-     */
-    public Node<E> next()
-    {
-        return next;
-    }
-
-
-    /**
-     * Gets the node before this one
-     *
-     * @return the node before this one
-     */
-    public Node<E> previous()
-    {
-        return previous;
-    }
-
-
-    /**
-     * Gets the data in the node
-     *
-     * @return the data in the node
-     */
-    public E getData()
-    {
-        return data;
-    }
-
-
-
+    
 
 
 
