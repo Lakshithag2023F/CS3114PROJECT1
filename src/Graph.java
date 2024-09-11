b9467ddf2281d6bcdf447e83bc83644f32370c23
shrutin3009 <139425957+shrutin3009@users.noreply.github.com>
2024-09-11 19:40:04 -0400

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -1,8 +1,86 @@
 public class Graph {
-    //~ Fields ................................................................
+    // ~ Fields ................................................................
+    private DoubleLL<Node>[] vertex;
+    private int numberOfNodes;
+    private int size;
 
-    //~ Constructors ..........................................................
+    // ~ Constructors ..........................................................
+    public Graph(int startSize) {
+        this.size = startSize;
+        this.numberOfNodes = 0;
+        this.vertex = new DoubleLL[startSize];
+    }
 
-    //~Public  Methods ........................................................
+    // ~Public Methods ........................................................
 
+
+    /**
+     * adds a new node to the graph
+     */
+    public void newNode(Node node) {
+        // implement
+    }
+
+
+    /**
+     * adds an edge between two nodes
+     * 
+     * @param start
+     * @param end
+     */
+    public void addEdge(Node start, Node end) {
+        // implement
+    }
+
+
+    /**
+     * check if edge exists
+     */
+    public boolean hasEdge(Node start, Node end) {
+        // implement
+        return false;
+    }
+
+
+    /**
+     * removes edge between nodes
+     */
+    public void removeEdge(Node start, Node end) {
+        // implemetn
+    }
+
+
+    /**
+     * remove node and edges
+     */
+    public void removeNode(Node node) {
+        // implemenet
+    }
+
+    /**
+     * expand size of adjacency list array
+     */
+    public void expand() {
+
+    }
+    
+    
+    /**
+     * print the graph
+     */
+    public void print() {
+        
+    }
+    
+    public void union() {
+        
+    }
+    
+    public void connectedComponent() {
+        
+    }
+    
+    public int diameter() {
+        return 0;
+    }
 }
