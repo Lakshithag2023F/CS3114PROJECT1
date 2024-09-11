0c6aa852951ad28b2993ac0949faffa9a8836f41
shrutin3009 <139425957+shrutin3009@users.noreply.github.com>
2024-09-11 19:15:21 -0400

    Update Record.java

--- src/Record.java
+++ src/Record.java
@@ -1,25 +1,58 @@
 public class Record {
     // ~ Fields ................................................................
-    private String key;
-    private Node node;
+    private String key; // artist name or track
+    private Node node; // associated node
     
     // ~ Constructors ..........................................................
 
     // ~Public Methods ........................................................
     
+    /**
+     * Constructor initialized record with key and node
+     * @param key
+     * @param node
+     */
     public Record(String key, Node node)
     {
         this.key = key;
         this.node = node;
     }
     
+    
+    // Getter methods
+    /**
+     * getter for key
+     * @return the key
+     */
     public String getKey()
     {
         return key;
     }
     
+    /**
+     * getter for node
+     * @return the node
+     */
     public Node getNode()
     {
         return node;
     }
+    
+    // setter methods
+    /**
+     * setter for key
+     * @param key
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+    
+    /**
+     * setter for node
+     * @param node
+     */
+    public void setNode(Node node) {
+        this.node = node;
+    }
+    
 }
