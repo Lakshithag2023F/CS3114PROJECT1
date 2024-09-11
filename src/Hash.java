1700641402beafdd78a2c9461f6538e520c77a2a
shrutin3009 <139425957+shrutin3009@users.noreply.github.com>
2024-09-11 16:42:58 -0400

    added the methods and fields for hash class, still needs to be implemented

--- src/Hash.java
+++ src/Hash.java
@@ -1,5 +1,9 @@
 /**
  * Hash table class
+ * 
+ * this class manages hash table data structure. hash table retrieves data using
+ * key-value pair system.
+ * manages how data is inserted, retrieved and removed from this table
  *
  * @author <Put Something Here>
  * @version <Put Something Here>
@@ -10,17 +14,62 @@ public class Hash {
     private boolean whichTable; // artist or song table
     private int numberOfRecords; // number of records
     private Record tombstone; // deleted records
-    
-    
+
     /**
      * Constructor
-     * @param size is the initial size of the hash table
+     * 
+     * @param size
+     *            is the initial size of the hash table
      */
     public Hash(int size) {
         allRecords = new Record[size];
         numberOfRecords = 0;
         tombstone = new Record(null, null);
     }
+
+
+    // public methods
+    /**
+     * Inserts new record into hash table
+     * 
+     * @param key
+     * @param value
+     */
+    public void insert(String key, Node value) {
+        // implement
+    }
+
+
+    /**
+     * Removes record
+     * 
+     * @param key
+     */
+    public void remove(String key) {
+        // implement
+    }
+
+
+    /**
+     * Find record index with its key
+     * 
+     * @param key
+     * @return integer value
+     */
+    public int find(String key) {
+        // implement
+
+        return 0;
+    }
+
+
+    /**
+     * Prints hash table contents
+     */
+    public void print() {
+        // implement
+    }
+
 
     /**
      * Compute the hash function
