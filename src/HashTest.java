3f32e25ea4a4ea8f1eb9d7dcf1cafee8f20eb078
Lakshithag2023F <148505659+Lakshithag2023F@users.noreply.github.com>
2024-09-15 19:42:41 -0400

    lhlkjkjh

--- src/HashTest.java
+++ src/HashTest.java
@@ -35,17 +35,8 @@ public class HashTest extends TestCase {
         int index = hash.find("hokies");
 
         Record[] allRecords = hash.getAllRecords();
-        // assertEquals("hokies", allRecords[index].getKey());
-        // doesnt have tombstone
+         assertEquals("hokies", allRecords[index].getKey());
 
-    }
-
-
-    public void testInsertTombstone() {
-        hash.insert(record);
-        hash.remove("hokies");
-        hash.insert(record);
-        // gets a tombstone
     }
 
 
@@ -78,44 +69,6 @@ public class HashTest extends TestCase {
     }
 
 
-    public void testInsertProbing() {
-        Node node5 = new Node(4);
-        Record record5 = new Record("one", node5);
-
-        Node node6 = new Node(4);
-        Record record6 = new Record("two", node6);
-
-        Node node7 = new Node(4);
-        Record record7 = new Record("three", node7);
-
-        Node node8 = new Node(4);
-        Record record8 = new Record("four", node8);
-
-        Node node9 = new Node(4);
-        Record record9 = new Record("five", node9);
-
-        Node node10 = new Node(4);
-        Record record10 = new Record("six", node10);
-
-        Node node11 = new Node(7);
-        Record record11 = new Record("seven", node11);
-
-        hash.insert(record5);
-        hash.insert(record6);
-        hash.insert(record7);
-        hash.insert(record8);
-        hash.insert(record9);
-        hash.insert(record10);
-        hash.insert(record11);
-
-        Record[] allRecords = hash.getAllRecords();
-
-        for (Record record : allRecords) {
-            System.out.print(record + " ");
-        }
-        System.out.println();
-
-    }
 
 
     public void testRehash() {
@@ -168,11 +121,28 @@ public class HashTest extends TestCase {
     }
 
 
-    public void testPrint() {
-        hash.insert(record);
-        // String printOutput = hash.print();
-    }
 
+
+    
+    public void testGetRecords()
+    {
+        hash.insert(record);
+        Record getIndex = hash.getRecord("hokies");
+        assertEquals(record, getIndex);
+        hash.getRecord("hokies");
+        assertNull( hash.getRecord("notThere"));
+
+        
+    }
+    
+    
+    public void testPrint() {
+        hash.print();
+        hash.insert(record);
+        hash.print();
+        hash.remove("hokies");
+        hash.print();
+    }
 
     /**
      * Check out the sfold method
@@ -191,4 +161,77 @@ public class HashTest extends TestCase {
         assertTrue(Hash.h("Long   Lonesome Blues", 10000) == 4159);
         assertTrue(Hash.h("long Lonesome Blues", 10000) == 4667);
     }
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    /**
+     * Test if the rehashing works.
+     */
+    public void testRehashingWorks() {
+        for (int i = 0; i < 6; i++) {
+            Node node = new Node(i);
+            Record record = new Record("key" + i, node);
+            hash.insert(record);
+        }
+
+        for (int i = 0; i < 6; i++) {
+            assertNotNull(hash.getRecord("key" + i));
+        }
+
+        Node nodeExtra = new Node(7);
+        Record recordExtra = new Record("keyExtra", nodeExtra);
+        hash.insert(recordExtra);
+
+        for (int i = 0; i < 6; i++) {
+            assertNotNull(hash.getRecord("key" + i));
+        }
+        assertNotNull(hash.getRecord("keyExtra")); // Ensure extra key is also inserted
+    }
+
+    
+    
+    
+    
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
